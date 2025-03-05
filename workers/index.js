ce1e84075234bfd596ec10592d41e5869c25872e
kepengqi <kepengqi@163.com>
2025-03-05 21:50:44 +0800

    wrangler config

--- workers/index.js
+++ workers/index.js
@@ -1,26 +1,65 @@
-import { handleNextRequest } from "../.open-next/server-functions/index.mjs";
-
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event));
+import { AsyncLocalStorage } from "node:async_hooks";
+// @ts-expect-error: resolved by wrangler build
+import * as nextEnvVars from "../.open-next/env/next-env.mjs";
+// @ts-expect-error: resolved by wrangler build
+import { handler as middlewareHandler } from "../.open-next/middleware/handler.mjs";
+// @ts-expect-error: resolved by wrangler build
+import { handler as serverHandler } from "../.open-next/server-functions/default/handler.mjs";
+const cloudflareContextALS = new AsyncLocalStorage();
+// Note: this symbol needs to be kept in sync with `src/api/get-cloudflare-context.ts`
+Object.defineProperty(globalThis, Symbol.for("__cloudflare-context__"), {
+  get() {
+    return cloudflareContextALS.getStore();
+  },
 });
-
-async function handleRequest(event) {
-  const url = new URL(event.request.url);
-  const cacheKey = url.pathname;
-
-  const cached = await MY_KV.get(cacheKey);
-    if (cached) {
-      console.log('走缓存');
-      
-    const headers = url.pathname.startsWith("/api/")
-      ? { "Content-Type": "application/json" }
-      : { "Content-Type": "text/html" };
-    return new Response(cached, { headers });
+// Populate process.env on the first request
+let processEnvPopulated = false;
+export default {
+  async fetch(request, env, ctx) {
+    return cloudflareContextALS.run({ env, ctx, cf: request.cf }, async () => {
+      const url = new URL(request.url);
+      populateProcessEnv(url, env.NEXTJS_ENV);
+      if (url.pathname === "/_next/image") {
+        const imageUrl = url.searchParams.get("url") ?? "";
+        return imageUrl.startsWith("/")
+          ? env.ASSETS.fetch(new URL(imageUrl, request.url))
+          : fetch(imageUrl, { cf: { cacheEverything: true } });
+      }
+      // The Middleware handler can return either a `Response` or a `Request`:
+      // - `Response`s should be returned early
+      // - `Request`s are handled by the Next server
+      const reqOrResp = await middlewareHandler(request, env, ctx);
+      if (reqOrResp instanceof Response) {
+        return reqOrResp;
+      }
+      return serverHandler(reqOrResp, env, ctx);
+    });
+  },
+};
+/**
+ * Populate process.env with:
+ * - the variables from Next .env* files
+ * - the origin resolver information
+ *
+ * Note that cloudflare env string values are copied by the middleware handler.
+ */
+function populateProcessEnv(url, nextJsEnv) {
+  if (processEnvPopulated) {
+    return;
+  }
+  processEnvPopulated = true;
+  const mode = nextJsEnv ?? "production";
+  if (nextEnvVars[mode]) {
+    for (const key in nextEnvVars[mode]) {
+      process.env[key] = nextEnvVars[mode][key];
     }
-      console.log("没走缓存");
-
-  const response = await handleNextRequest(event.request);
-  const body = await response.text();
-  event.waitUntil(MY_KV.put(cacheKey, body, { expirationTtl: 3600 }));
-  return response;
+  }
+  // Set the default Origin for the origin resolver.
+  process.env.OPEN_NEXT_ORIGIN = JSON.stringify({
+    default: {
+      host: url.hostname,
+      protocol: url.protocol.slice(0, -1),
+      port: url.port,
+    },
+  });
 }
